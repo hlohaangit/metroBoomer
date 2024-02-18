001d42068b90bca841b632cc3662428bd3376ed7
hlohaangit <hlohaan@gmail.com>
2024-02-17 23:04:26 -0500

    added impementation of metronome

--- metronome/main.cpp
+++ metronome/main.cpp
@@ -21,24 +21,9 @@ volatile bool btn_mode_pressed = false;
 
 // Run an additional loop separate from the main one.
 void blink() {
-	bool on = false;
-	// ** This loop manages LED blinking. **
-	while (true) {
-		// The LED state will toggle once every second.
-		std::this_thread::sleep_for(1s);
-
-		// Perform the blink if we are pressed,
-		// otherwise just set it to off.
-		if (btn_mode_pressed)
-			on = !on;
-		else
-			on = false;
-
-		// HIGH/LOW probably equal 1/0, but be explicit anyways.
-		// digitalWrite(LED_RED, (on) ? HIGH : LOW);
-		gpioWrite(LED_RED, (on) ? 1 : 0);
-
-	}
+	gpioWrite(LED_RED, 1);
+	std::this_thread::sleep_for(1s);
+	gpioWrite(LED_RED, 0);
 }
 
 // This is called when a GET request is made to "/answer".
@@ -46,10 +31,39 @@ void get42(web::http::http_request msg) {
 	msg.reply(200, web::json::value::number(42));
 }
 
+// --------------------metronome implementations----------------------------
+void metronome::start_timing() {
+	m_timing = true;
+	m_beat_count = 0;
+}
+
+void metronome::stop_timing() {
+	m_timing = false;
+}
+
+void metronome::tap() {
+	if (m_timing && m_beat_count < beat_samples) {
+		auto now = std::chrono::system_clock::now().time_since_epoch().count();
+		m_beats[m_beat_count] = now;
+	}
+}
+
+size_t metronome::get_bpm() const {
+	if (m_beat_count < beat_samples)
+		return 0;
+
+	size_t sum = 0;
+	for (size_t i = 0; i < beat_samples - 1; ++i)
+		sum += m_beats[i + 1] - m_beats[i];
+	return 6000000000 / (sum / (beat_samples - 1));
+
+}
+
 // This program shows an example of input/output with GPIO, along with
 // a dummy REST service.
 // ** You will have to replace this with your metronome logic, but the
 //    structure of this program is very relevant to what you need. **
+
 int main() {
 	// WiringPi must be initialized at the very start.
 	// This setup method uses the Broadcom pin numbers. These are the
@@ -59,12 +73,22 @@ int main() {
 		return 1;
 	}
 
+	// ** This is where you would create an instance of your metronome. **
+	metronome my_metronome;
+	my_metronome.start_timing();
+
+
+	// simulate beat tapping
+	// for (int i = 0; i < metronome::beat_samples; ++i) {
+	// 	metronome.tap();
+	// 	blink();
+	// 	std::this_thread::sleep_for(1s);
+	// }
+
 	// Set up the directions of the pins.
 	// Be careful here, an input pin set as an output could burn out.
-	// pinMode(LED_RED, OUTPUT);
 	gpioSetMode(LED_RED, PI_OUTPUT);
 	gpioSetMode(LED_GREEN, PI_OUTPUT);
-	// pinMode(BTN_MODE, INPUT);
 	gpioSetMode(BTN_MODE, PI_INPUT);
 	gpioSetMode(BTN_TAP, PI_INPUT);
 	// Note that you can also set a pull-down here for the button,
@@ -83,23 +107,19 @@ int main() {
 	// Use a separate thread for the blinking.
 	// This way we do not have to worry about any delays
 	// caused by polling the button state / etc.
-	std::thread blink_thread(blink);
-	blink_thread.detach();
-
-	// ** This loop manages reading button state. **
-	while (true) {
-		// We are using a pull-down, so pressed = HIGH.
-		// If you used a pull-up, compare with LOW instead.
-		// btn_mode_pressed = (digitalRead(BTN_MODE) == HIGH);
-		btn_mode_pressed = (gpioRead(BTN_MODE) == 1);
-		// Delay a little bit so we do not poll too heavily.
-		std::this_thread::sleep_for(10ms);
-
-		// ** Note that the above is for testing when the button
-		// is held down. For detecting "taps", or momentary
-		// pushes, you need to check for a "rising edge" -
-		// this is when the button changes from LOW to HIGH... **
+	for (int i =0; i< metronome::beat_samples; ++i) {
+		my_metronome.tap();
+		std::thread blink_thread(blink);
+		std::this_thread::sleep_for(1s);
 	}
+
+	my_metronome.stop_timing();
+	size_t bpm = my_metronome.get_bpm();
+	std::cout << "BPM: " << bpm << std::endl;
+
+	// blink_thread.detach();
+
+	gpioTerminate();
 
 	return 0;
 }
--- metronome/metronome.hpp
+++ metronome/metronome.hpp
@@ -5,30 +5,120 @@
 class metronome
 {
 public:
-	enum { beat_samples = 3 };
+	enum { beat_samples = 4 };
 
 public:
 	metronome()
-	: m_timing(false), m_beat_count(0) {}
+	: m_timing(false), m_beat_count(0) {
+		for (int i=0; i< beat_samples; i++) m_beats[i]=0;
+	}
 	~metronome() {}
 
 public:
 	// Call when entering "learn" mode
-	void start_timing();
+	void start_timing(){
+		printf("Timing Start \n");
+		m_timing = true;
+		firstTap = lastTap = 0;
+		m_beat_count = 0;
+	}
+
+
 	// Call when leaving "learn" mode
-	void stop_timing();
+	void stop_timing() {
+		printf("Timing Stop \n");
+		m_timing = false;
+
+		if (m_beat_count >=4) {
+			for (int i = 0; i< beat_samples-1; i++) {
+				m_beats[i] = m_beats[i+1];
+			}
+
+			double seconds = ((double)(lastTap - firstTap) / (double) 1000);
+			if (seconds <= 0){
+				printf("there was as error: lastTap=%u, firstTap=%u, seconds=%u\n", lastTap, firstTap, seconds);
+				m_beats[beat_samples-1] = 0;
+			}
+			else {
+				m_beats[beat_samples-1] = size_t ((double) m_beat_count * ((double)60 / seconds));
+			}
+			printf("New BPM = %u\n", m_beats[beat_samples-1]);
+		}
+		else {
+			printf("issue in recording BPM\nAt least 4 taps required for measurement")
+		}
+	}
 
 	// Should only record the current time when timing
 	// Insert the time at the next free position of m_beats
-	void tap();
+	void tap(){
+		size_t time_stamp = millis();
+		if (firstTap ==0){
+			firstTap = time_stamp;
+		}
+		lastTap = time_stamp;
+		m_beat_count++
+
+		//logs
+		printf("beat count: %d\n", m_beat_count);
+		printf ("m_beats=[");
+		for(int j = 0 ; j < beat_samples ; j++)
+			printf("%u, ", m_beats[j]);
+		printf("]\n");
+	}
 
 	bool is_timing() const { return m_timing; }
 	// Calculate the BPM from the deltas between m_beats
 	// Return 0 if there are not enough samples
-	size_t get_bpm() const;
+	size_t get_bpm() const {
+		printf("Current bpm = %u\n", m_beats[beat_samples-1]);
+		return m_beats[beat_samples-1];
+	}
+
+	size_t getMIN_MAX(int mode) const {
+		size_t ans;
+		ans = m_beats[0];
+		//mode 0 for get MIN, mode 1 for get MAX
+		if (mode == 0) {
+			for(int i = 1 ; i < beat_samples ; i++){
+				if(m_beats[i] < ans) {
+					ans = m_beats[i];
+				}
+			}
+			return ans;
+		} else {
+			for(int i = 1 ; i < beat_samples ; i++){
+				if(m_beats[i] > ans) {
+					ans = m_beats[i];
+				}
+			}
+			return ans;
+		}
+		return 0;
+	}
+
+	void delMIN_MAX(int mode) {
+		size_t ans = getMIN_MAX(mode);
+		for (int i = 0; i< beat_samples ; i++){
+			if(m_beats[i] == ans)
+				m_beats[i] = 0;
+		}
+	}
+
+	//replace the new BPM with the oldest one
+	void addNewBPM(size_t val) {
+		for(int i = 0 ; i < beat_samples-1 ; i++) {
+				m_beats[i] = m_beats[i+1];
+		}
+		m_beats[beat_samples-1] = val;
+	}
+
+
 
 private:
 	bool m_timing;
+	size_t firstTap;
+	size_t lastTap;
 
 	// Insert new samples at the end of the array, removing the oldest
 	size_t m_beats[beat_samples];
